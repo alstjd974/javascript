9d9c650e44e7293c8654a8e8e0aba2acdc9dd4b7
alstjd974 <alstjd974@nate.com>
2022-02-19 01:14:44 +0900

    2.2
    Variables

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,8 @@
-alert("hi");
+const a = 5;
+const b = 2; // intger 
+const myName = "min sung"; //string
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + " " + myName);
