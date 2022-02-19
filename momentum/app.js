7a2553bdbf3beb8d7cea46f9e7e9fb0bf49d6cf4
alstjd974 <alstjd974@nate.com>
2022-02-19 23:46:13 +0900

    2.3
    always : const / sometimes : let / never : var

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,13 @@
-const a = 5;
-const b = 2; // intger 
-const myName = "min sung"; //string
+// always : const / sometimes : let / never :var
+const a = 5; 
+const b = 2; // intger  변환 불가능 함수 
+let myName = "min sung"; //string  변환가능 함수
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello" + " " + myName);
+
+myName = "nico";
+
+console.log("your name is" + " " + myName );
