231bb6f2d191f745487f37866e7f0c44ef6c5eef
alstjd974 <alstjd974@nate.com>
2022-02-21 20:38:56 +0900

    2.4 Booleans
    ture : on , false : off  ,
    null :  nothing(비어있음) ,
    undefined : variable이 메모리에 만들어 졌고 컴퓨터가 이 variable에 대해서
    인지하고 있지만, 값이 없는 것

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,3 @@
-// always : const / sometimes : let / never :var
-const a = 5; 
-const b = 2; // intger  변환 불가능 함수 
-let myName = "min sung"; //string  변환가능 함수
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello" + " " + myName);
-
-myName = "nico";
-
-console.log("your name is" + " " + myName );
+const amIFat = null;
+let something;
+console.log(something, amIFat); 
