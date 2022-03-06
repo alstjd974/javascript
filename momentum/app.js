efd7508dbb5154f48a333865e5758b09155aabae
alstjd974 <alstjd974@nate.com>
2022-03-06 17:10:47 +0900

    Array
    array(배열) : 하나의 variale 안에 데이터의 List를 가지는

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,20 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat); 
+/*
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sta = "sta";
+const sun = "sun";
+const dayofweek = {mon,tue,wed,thu,fri,sta,sun};
+*/
+
+const daysOfweek = ["mon", "tue", "wed", "thu", "fri", "sta"];
+
+// Get Item from Array
+console.log(daysOfweek);
+console.log(daysOfweek[4]);
+
+// Add one more day to the array
+daysOfweek.push("sun");
+console.log(daysOfweek);
