b7271194dadd8e29d62471c14e2c8f6adf797e2d
alstjd974 <alstjd974@nate.com>
2022-07-06 03:44:15 +0900

    2.8 function 2
    1. 인자를 통해 정보를 주고 받는다.
    2. 블록 안의 인자는 블록 안에서만 사용이 가능하다.

--- momentum/app.js
+++ momentum/app.js
@@ -1,10 +1,24 @@
-function sayHello(nameOfPerson, age) { 
-    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+function plus(firstNumber, secondNumber){
+    console.log(firstNumber + secondNumber);
+    //firstNumber = 2 secondNumber = 3
 }
 
+function divide (a,b){
+    console.log(a/b);
+    // a = 80 b = 25
+}
 
-sayHello("min",10);
-sayHello("sung",20);
+plus(2 , 3);
+divide(80,25);
 
-// function은 어떤 코드를 캡슐화해서, 실행을 여러 번 할 수 있게 해준다.
-// argument(인수)는, funtion을 실행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법
+const player = {
+    name: "min sung",
+    sayHello : function(otherPersonName){
+        console.log("Hello " + otherPersonName + " nice to meet you!");
+    }
+};
+
+console.log(player.name);
+player.sayHello("Lee");
+player.sayHello("min");
+
