ba85238240dec99ee47c1203439f2de3c900ea76
alstjd974 <alstjd974@nate.com>
2022-07-07 06:14:35 +0900

    2.13 type (string ->parseInt)
    function인 parseInt는 입력 받은 값의 타입을 number 바꾸어 준다.

--- momentum/app.js
+++ momentum/app.js
@@ -1,26 +1,5 @@
-const calculator = {
-    add : function(a,b) {
-         a+b;
-    },
-    minus : function(a,b) {
-        return a-b;
-    },
-    div : function(a,b) {
-        return a/b;
-    },
-    pow : function(a,b) {
-        return a**b;
-    }
+const stringAge = prompt("How old are you?");
+const numberAge = parseInt( prompt("How old are you?") );
 
-};
-
-const addResult = calculator.add(10,4);
-const minusResult = calculator.minus(addResult,2);
-const divResult = calculator.div(minusResult,2);
-const powResult = calculator.pow(divResult,2);
-
-console.log(calculator.add(2,2));
-// console.log(addResult);
-// console.log(minusResult);
-// console.log(divResult);
-// console.log(powResult);
+console.log(typeof stringAge);
+console.log(typeof numberAge);
