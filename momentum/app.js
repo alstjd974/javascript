9d81f1be37e94494a813e4aab490543674fe3767
alstjd974 <alstjd974@nate.com>
2022-07-07 18:42:48 +0900

    2.16 operater ===

--- momentum/app.js
+++ momentum/app.js
@@ -13,6 +13,11 @@ else if(age >= 20 && age <= 50 ){
 else if(age > 50  && age <= 80){
     console.log("you should exercise");
 }
+else if(age === 100){
+    console.log("wow you are wise!");
+}
 else if(age > 80){
     console.log("you can do whatever you want.");
 }
+
+if ((a && b) || (c && b) || (x || y) );
