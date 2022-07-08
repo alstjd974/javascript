9bb72b92e081e01c8ac322b98efafc3749ec9776
alstjd974 <alstjd974@nate.com>
2022-07-09 01:09:30 +0900

    serching For Elements
    1. querySeletor는 element를 CSS 방식으로 검색할 수 있다. 즉 hello란 class 내부의 h1을 가지고 올 수 있다는 의미
    2.  Id를 querySeletor로도 가져올 수 있음
    const title2 = document.querySelector(#id명);

--- momentum/app.js
+++ momentum/app.js
@@ -1,6 +1,12 @@
-const title = document.getElementById("title");
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos);
 
-title.innerText = "Got you";
-title.autofocus = true;
-title.className = "hello";
-console.dir(title);
+const title = document.getElementsByTagName("h1");
+console.log(title);
+
+const title2 = document.querySelector(".hello h1");
+title2.innerText = "hello";
+console.log(title2);
+
+const title3 = document.querySelectorAll(".hello h1");
+console.log(title3);
