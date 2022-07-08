e887cc01a2f00216fcdbbc8481181c6beae7a880
alstjd974 <alstjd974@nate.com>
2022-07-08 20:36:39 +0900

    3.1 HTML in Javascript
    1. id를 이용하여 document의 getElementById함수로 h1(element)를 가져옴
    2. javascript에서 title이라는 항목을 가져왔다.
    3.  javascript로 HTML의 요소를 읽고 변경이 가능하다.

--- momentum/app.js
+++ momentum/app.js
@@ -1,23 +1,6 @@
-const age = parseInt( prompt("How old are you") );
+const title = document.getElementById("title");
 
-
-if(isNaN(age) || age < 0){
-    console.log("please write a number");
-}
-else if(age < 20){
-    console.log("you are too young");
-}
-else if(age >= 20 && age <= 50 ){
-    console.log("you can drink");
-}
-else if(age > 50  && age <= 80){
-    console.log("you should exercise");
-}
-else if(age === 100){
-    console.log("wow you are wise!");
-}
-else if(age > 80){
-    console.log("you can do whatever you want.");
-}
-
-if ((a && b) || (c && b) || (x || y) );
+title.innerText = "Got you";
+title.autofocus = true;
+title.className = "hello";
+console.dir(title);
