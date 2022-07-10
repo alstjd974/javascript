b22ea60fba243be250603e826f62488dafddb5cd
alstjd974 <alstjd974@nate.com>
2022-07-11 01:01:54 +0900

    3.5 Event 2
    h1이 아닌 window의 element인 event를 listen
    document.body(title,head) 가져올 수 있다. 나머지는 querySeletor , getElementsById 등 으로 가져온다.

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,38 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 
 function handleTitleMouseenter() {
-  title.innerText = "Mouse is here!";
+  h1.innerText = "Mouse is here!";
 }
 
 function handleTitleMouseLeave() {
-  title.innerText = "Mouse is gone~";
+  h1.innerText = "Mouse is gone~";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleTitleMouseenter);
-title.addEventListener("mouseleave", handleTitleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("All GOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleTitleMouseenter);
+h1.addEventListener("mouseleave", handleTitleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
