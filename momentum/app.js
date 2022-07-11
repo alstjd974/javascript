d7992bf96848619050bf4338db7ce0c7a03df033
alstjd974 <alstjd974@nate.com>
2022-07-11 12:24:13 +0900

    3.6
    1. element 찾아오기
    2. event
    3. 반응

--- momentum/app.js
+++ momentum/app.js
@@ -1,38 +1,14 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
-}
-
-function handleTitleMouseenter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleTitleMouseLeave() {
-  h1.innerText = "Mouse is gone~";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("All GOOD");
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleTitleMouseenter);
-h1.addEventListener("mouseleave", handleTitleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
