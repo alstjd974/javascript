d647b61f51e1e4e3338d5db92f390bb051cc57eb
alstjd974 <alstjd974@nate.com>
2022-07-20 15:46:22 +0900

    Loading Username
    1.조건문을 통해 local storage에 username 유무 판단 후
    
    2.username이 null일 경우 loginForm의 hidden class를 제거하고 나타나게 한다.
    
    3. username 이 있을 경우 greeting의 hidden class를 제거하고 나타나게 한다.

--- momentum/app.js
+++ momentum/app.js
@@ -3,16 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   localStorage.setItem("username", username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  console.log(username);
-  //greeting.innerText = "Hello " + username;
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
+}
