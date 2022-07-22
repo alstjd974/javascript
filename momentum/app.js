b6365dccbce6864ab08e54f630d9f52c87fb1866
alstjd974 <alstjd974@nate.com>
2022-07-23 00:58:01 +0900

    4.7 Super Recap
    local storage가 정보를 저장하고 불러오고 삭제하는
    브라우저가 가지고 있는 작은 DB같은 API
    
    paintGreetings : 화면에 텍스트가 출력됙 하는 function
    paintGreetings 함수를 호출할 때, 다른 이름의 인자를 넣고 있다.
    1. onLoginSubmit 함수에서는 유저 정보고 input으로부터 오고 있다.
    2. 유저정보가 local storage로 부터 오고 있다.
    
    paintGreetings 함수는 따로 인자를 받을 필요가 없다
    이유 :  local storage에 유저정보가 존재하는걸 알고 있기 때문이다.
    
    paintGreetings 함수 안에 username이라는 변수를 만들고
    local storage에 있는 username을 찾도록 한다.
    
    local storage를 두번 열어본다.

--- momentum/app.js
+++ momentum/app.js
@@ -7,18 +7,18 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  paintGreetings(username);
+  paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem("username");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   // show the form
@@ -26,5 +26,5 @@ if (savedUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   // show the greetings
-  paintGreetings(savedUsername);
+  paintGreetings();
 }
